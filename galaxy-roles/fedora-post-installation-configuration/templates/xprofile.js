a60347e8672059c69520209d67cac37abf85ac8b
Dallas Hall <mr.dallas.hall@gmail.com>
2021-10-28 19:22:32 +1100

    Adding in more crap I don't want to do.

---
+++ galaxy-roles/fedora-post-installation-configuration/templates/xprofile.js
@@ -0,0 +1,7 @@
+# Reload nvidia settings, see 'man nvidia-settings'
+# https://download.nvidia.com/XFree86/Linux-x86_64/1.0-6106/nvidia-settings-user-guide.txt
+# https://wiki.archlinux.org/index.php/NVIDIA#NVIDIA_Settings
+nvidia-settings --load-config-only
+
+# https://wiki.archlinux.org/title/Mouse_acceleration#Using_xset
+xset m 0 0
